edc14b53fd93cb6dade8db8e18a5f04a577a100b
baldrailers <baldrailers@gmail.com>
2009-09-29 14:34:25 +0800

    Adding couchlog.yml

--- install.rb
+++ install.rb
@@ -1,1 +1,7 @@
-# Install hook code here
+require 'fileutils'
+
+#copy couchlog_sample.yml to config directory
+couchlog_config = File.dirname(__FILE__) + '/../../../config/couchlog.yml'
+unless File.exist?(couchlog_config)
+  FileUtils.copy(File.dirname(__FILE__) + '/couchlog.yml.sample',couchlog_config)
+end
--- lib/couch_log.rb
+++ lib/couch_log.rb
@@ -1,5 +1,7 @@
 require 'active_support'
 require 'couchrest'
+require 'yaml'
+require 'fileutils'
 
 class CouchLog < ActiveSupport::BufferedLogger
   
@@ -23,19 +25,29 @@ class CouchLog < ActiveSupport::BufferedLogger
       :UNKNOWN  
     end  
   end  
+  
+  # load config/couchlog.yml
+  def load_configuration
+    
+  end
    
   public  
    
   # monkey patch the CustomLogger add method so that  
   # we can format the log messages the way we want  
   def add(severity, message = nil, progname = nil, &block)  
+    path = "#{RAILS_ROOT}/config/couchlog.yml"
+    return unless File.exists?(path)
+    
+    conf = YAML::load(ERB.new(IO.read(path)).result)[RAILS_ENV]
+    
     return if @level > severity  
     message = (message || (block && block.call) || progname).to_s  
 
     # Log everything to CouchDB
     # Don't show anything on the command or in the log files
     # Format = { DateTime, Severity, Message }
-    db = CouchRest.database!("http://127.0.0.1:5984/couch_log_lib_test")
+    db = CouchRest.database!("http://#{conf[:host]}:#{conf[:port]}/#{conf[:database]}")
     message = db.save_doc({
       "DateTime" => Time.now.strftime("%m/%d/%Y %H:%M:%S"),
       "Severity" => severity_string(severity),
